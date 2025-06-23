aaf5ad6ab7e0ccf3ed7c5c72c1d7f99131ede0b6
Aluno <igor.s10@aluno.ifsc.edu.br>
2025-06-23 09:21:20 -0300

    Realizados testes com alguns métodos da interface DrawListener e preparado alguns escopos.

---
+++ app/src/main/java/ifsc/poo/Constantes.java
@@ -0,0 +1,20 @@
+// Classe para constantes
+// SE necessário...
+
+/*
+ * FUNÇÃO DE CADA TECLA:
+ * 
+ * F1 : Círculo
+ * F2 : Quadrado
+ * F3 : Hexágono
+ * F4 : Pentágono
+ * F5 : Vermelho
+ * F6 : Azul
+ * F7 : Verde
+ * F8 : Amarelo
+ * C  : Limpa o tabuleiro
+ * F  : Alterna entre figura vazada ou preenchida
+ * P  : Imprime informações das figuras
+ * Q  : Diminui tamanho da figura
+ * W  : Aumenta tamanho da figura
+*/
